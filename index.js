7dd2bb5d9553c899badafc648b73818e88057628
Helin Faidhan <helinkurd1997@gmail.com>
2022-09-03 21:42:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue)
+
+  { let result
+    if (someValue <= 400) {
+      'this is on me '}
+    
+      else if(someValue > 400 && someValue < 2000 ){
+        'this is 20 $'
+      }
+      else if (someValue > 2000){
+        'this is 30 $'
+      }
+      else if ( someValue > 2500) {
+        'this is not allow '
+      }
+    
+      return result
+    }
+  
+
+  
+
+
+function ternaryCheckCity(someText){
+  let result 
+  if ( someText = NYC){
+    'Ok, sounds good.'
+  }
+  else{ 'no go'}
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(customers){
+  let result
+  if(customers=generous){
+    'Thank you so much.'
+  }
+  else if (customers=notgenerous){
+    'thank you'
+  }
+  else{'bye'}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  return result
 }
